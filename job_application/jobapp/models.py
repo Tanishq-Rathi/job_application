ae212349836683c657d66aed59182d5d0919616e
Tanishq-Rathi <tanishqrathi2509@gmail.com>
2024-05-01 19:30:22 +0530

    Job recommender
    complete

--- job_application/jobapp/models.py
+++ job_application/jobapp/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class JobApplication(models.Model):
     job_title = models.CharField(max_length=100)
-    recent_degree = models.CharField(max_length=100)
-    specialization = models.CharField(max_length=100)
+    degree_Type = models.CharField(max_length=100)
+    Total_Year_of_Experience = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     currently_working = models.BooleanField(default=False)
     current_ctc = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
